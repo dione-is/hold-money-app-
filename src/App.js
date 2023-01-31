9c7636cfa43247edbe820ae4f34062e2a4120296
dione.santos <ra-20017086-2@alunos.unicesumar.edu.br>
2023-01-30 23:18:33 -0300

    component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h4>hello World</h4>
-      <h4>hello World</h4>
-      <h4>hello World</h4>
-    </div>
-  );
+class App extends React.Component {
+  state =  {
+    nome :'Dione Santos'
+  }
+  render() {
+    return (
+      <div>
+        <label>Nome:</label>
+        <input type="text" value={this.state.nome} onChange={(e) => this.setState({nome: e.target.value})}/>
+        o nome digitado foi: {this.state.nome}
+      </div>
+    );
+  }
 }
+
 
 export default App;
